3cc6e43e710b31ebb2bb67b3a68c257df5482bf4
ADI SURYA BASRI <54743290+adisurya1997@users.noreply.github.com>
2022-11-07 10:01:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -15,5 +15,11 @@ api_base_url = "https://dev-bhagaskarash4zl.microgen.id/api" + api_key
 @app.get("/products")
 def handler():
     return "<h1>test</h1>"
+@app.post("/products")
+def handler():
+    limit = posts['limit']
+    keyword = posts['keyword']
+    return keyword
+
 if __name__ == "__main__":
     app.run(debug=True)
