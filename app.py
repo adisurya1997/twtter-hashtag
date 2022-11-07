d3efb02f5b6aeaaf1c3410832ef344745b9c686e
ADI SURYA BASRI <54743290+adisurya1997@users.noreply.github.com>
2022-11-07 09:29:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,6 @@ api_base_url = "https://api.stagingv3.microgen.id/query/api/v1/" + api_key
 
 @app.route('/')
 def handler(request):
-    return <h1>test</h1>
+    return "<h1>test</h1>"
 if __name__ == "__main__":
     app.run(debug=True)
