18c3b780aa2e5ab9ac1096bb168cff27084639cb
ADI SURYA BASRI <54743290+adisurya1997@users.noreply.github.com>
2022-11-07 10:26:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ api_base_url = "https://dev-bhagaskarash4zl.microgen.id/api" + api_key
 def handler():
     return "<h1>test</h1>"
 
-@app.post("/products")
+@app.post("/hashtag")
 def hashtag():
     try:
         print(request.data)
@@ -74,6 +74,63 @@ def hashtag():
             "error": e
         }
         return result
+    
+@app.post("/listpost")
+def listpost():
+    try:
+        print(request.data)
+        posts = json.loads(request.data)
+        limit = posts['limit']
+        keyword = posts['keyword']
 
+        tweets = []
+
+        scraper = twitterScrapper.TwitterListPostsScraper(keyword)
+        for i, tweet in enumerate(scraper.get_items()):
+
+            if i > limit:
+                break
+            print(f"{i} content: {tweet}")
+            tweets.append({"id": tweet.id,
+                           "username": tweet.user.username,
+                           'content': tweet.content,
+                           'url': tweet.url,
+                           'date': tweet.date,
+                           'renderedContent': tweet.renderedContent,
+                           'replyCount': tweet.replyCount,
+                           'retweetCount': tweet.retweetCount,
+                           'likeCount': tweet.likeCount,
+                           'quoteCount': tweet.quoteCount,
+                           #    'conversationId': tweet.conversationId,
+                           'source': tweet.source,
+                           'sourceUrl': tweet.sourceUrl,
+                           'sourceLabel': tweet.sourceLabel,
+                           'sourceLabel': tweet.sourceLabel,
+                           #    'outlinks': jsonify(tweet.outLinks),
+                           #    'tcooutlinks': tweet.tcooutLinks,
+                           'media': tweet.media,
+                           #    'retweetedTweet': json.dumps(tweet.retweeetTweet),
+                           #    'quotedTweet': tweet.quotedTweet,
+                           'inReplyToTweetId': tweet.inReplyToTweetId,
+                           #    'inReplyToUser': tweet.inReplyToUsers,
+                           #    'mentionedUsers': tweet.mentionedUsers,
+                           'coordinates': tweet.coordinates,
+                           'place': tweet.place,
+                           #    'hashtags': tweet.hastags,
+                           'cashtags': tweet.cashtags,
+                           #    'link': tweet.link,
+                           #    'search': tweet.seacrh,
+                           'likeCount': tweet.likeCount,
+                           'retweetCount': tweet.retweetCount,
+                           'lang': tweet.lang
+                           })
+        # j = json.dumps(tweets)
+        return jsonify(tweets)
+    except Exception as e:
+        result = {
+            "statusCode": 400,
+            "error": e
+        }
+        return result
 if __name__ == "__main__":
     app.run(debug=True)
