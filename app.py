6c9d90fc6562ad6077fe50e58865bdd02d245a01
ADI SURYA BASRI <54743290+adisurya1997@users.noreply.github.com>
2022-11-07 09:30:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 import requests
 from sys import stderr
 from flask import Flask, request, jsonify
-import sys
+
 app = Flask(__name__)
 
 api_key = os.environ.get("API_KEY", "")
@@ -13,7 +13,7 @@ if api_key == "":
 api_base_url = "https://api.stagingv3.microgen.id/query/api/v1/" + api_key
 
 @app.route('/')
-def handler(request):
+def handler():
     return "<h1>test</h1>"
 if __name__ == "__main__":
     app.run(debug=True)
