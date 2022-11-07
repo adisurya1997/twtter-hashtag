5b4594d14be1f8b59ac7befc10e52ab29335c2de
ADI SURYA BASRI <54743290+adisurya1997@users.noreply.github.com>
2022-11-07 10:36:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -185,5 +185,86 @@ def search():
             "error": e
         }
         return result
+@app.post("/trend")
+def trend():
+        try:
+        tweets = []
+
+        scraper = twitterScrapper.TwitterTrendsScraper()
+        for i, tweet in enumerate(scraper.get_items()):
+            print(tweet)
+            if i > 23:
+                break
+            print(f"{i} content: {tweet.name}")
+            tweets.append({"name": tweet.name,
+                           "domainContext": tweet.domainContext,
+                           'totalTweets': tweet.metaDescription,
+                           })
+
+        j = json.dumps(tweets)
+        return j
+    except Exception as e:
+        result = {
+            "statusCode": 400,
+            "error": e
+        }
+        return result
+
+@app.post("/user")
+def user():
+        try:
+        print(request.data)
+        posts = json.loads(request.data)
+        limit = posts['limit']
+        keyword = posts['account']
+        tweets = []
+
+        scraper = twitterScrapper.TwitterUserScraper(keyword)
+        for i, tweet in enumerate(scraper.get_items()):
+            print(tweet)
+            if i > limit:
+                break
+            print(f"{i} content: {tweet.content}")
+            tweets.append({"id": tweet.id,
+                           "username": tweet.user.username,
+                           'content': tweet.content,
+                           'url': tweet.url,
+                           'date': tweet.date,
+                           'renderedContent': tweet.renderedContent,
+                           'replyCount': tweet.replyCount,
+                           'retweetCount': tweet.retweetCount,
+                           'likeCount': tweet.likeCount,
+                           'quoteCount': tweet.quoteCount,
+                           #    'conversationId': tweet.conversationId,
+                           'source': tweet.source,
+                           'sourceUrl': tweet.sourceUrl,
+                           'sourceLabel': tweet.sourceLabel,
+                           'sourceLabel': tweet.sourceLabel,
+                           #    'outlinks': jsonify(tweet.outLinks),
+                           #    'tcooutlinks': tweet.tcooutLinks,
+                           'media': tweet.media,
+                           #    'retweetedTweet': json.dumps(tweet.retweeetTweet),
+                           #    'quotedTweet': tweet.quotedTweet,
+                           'inReplyToTweetId': tweet.inReplyToTweetId,
+                           #    'inReplyToUser': tweet.inReplyToUsers,
+                           #    'mentionedUsers': tweet.mentionedUsers,
+                           'coordinates': tweet.coordinates,
+                           'place': tweet.place,
+                           #    'hashtags': tweet.hastags,
+                           'cashtags': tweet.cashtags,
+                           #    'link': tweet.link,
+                           #    'search': tweet.seacrh,
+                           'likeCount': tweet.likeCount,
+                           'retweetCount': tweet.retweetCount,
+                           'lang': tweet.lang
+                           })
+        return jsonify(tweets)
+    except Exception as e:
+        result = {
+            "statusCode": 400,
+            "error": e
+        }
+        return result
+    
 if __name__ == "__main__":
     app.run(debug=True)
